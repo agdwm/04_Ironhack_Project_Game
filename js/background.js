155f08f674d912e9af7083bfdb53fd1d6507cfad
agdwm <almudenaguerraswm@gmail.com>
2018-09-08 20:37:42 +0200

    Drawn the background in the canvas

---
+++ js/background.js
@@ -0,0 +1,25 @@
+function Background(game) {
+	this.game = game;
+	this.img = new Image();
+	this.img.src = 'images/background.png';
+
+	this.w = 3200;
+	this.h = 600;
+	this.x = 0;
+	this.y = this.game.canvas.height - this.h;
+
+	this.dx = 2;
+}
+
+Background.prototype.draw = function () {
+	// two backgrounds: one after the other
+	this.game.ctx.drawImage(this.img, this.x, this.y, this.game.canvas.width, this.h);
+	this.game.ctx.drawImage(this.img, this.x + this.game.canvas.width, this.y, this.game.canvas.width, this.h);
+}
+
+
+Background.prototype.move = function () {
+	this.x -= this.dx;
+
+	if (this.x < -this.game.canvas.width) this.x = 0;
+};
--- js/game.js
+++ js/game.js
@@ -2,12 +2,37 @@ function Game (canvasId) {
 	this.canvas = document.getElementById(canvasId);
 	this.ctx = this.canvas.getContext('2d');
 	this.fps = 60;
+
+	this.reset();
 }
 
 Game.prototype.reset = function() {
-
+	this.background = new Background(this);
 }
 
-Game.prototype.start = function() {
+Game.prototype.clear = function () {
+	this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+};
 
+Game.prototype.start = function() {
+	this.interval = setInterval(() => {
+		this.clear();
+		this.framesCounter++;
+
+		if (this.framesCounter > 1000) {
+			this.framesCounter = 0;
+		}
+
+		this.moveAll();
+		this.draw();
+
+	}, 1000 / this.fps);
+}
+
+Game.prototype.draw = function () {
+	this.background.draw();
+}
+
+Game.prototype.moveAll = function () {
+	this.background.move();
 }
