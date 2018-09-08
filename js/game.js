a81ca46771b812daa5c9c9d6bf522b36b83695f5
agdwm <almudenaguerraswm@gmail.com>
2018-09-08 16:02:53 +0200

    Added styles to the home animation and generate the canvas

--- js/game.js
+++ js/game.js
@@ -1,3 +1,15 @@
-function Game(){
-	
+function Game(canvasId) {
+	this.canvas = document.getElementById(canvasId);
+	this.canvas.width = window.innerWidth;
+	this.canvas.height = window.innerHeight;
+	this.ctx = this.canvas.getContext('2d');
+	this.fps = 60;
+}
+
+Game.prototype.reset = function(){
+
+}
+
+Game.prototype.start = function(){
+
 }
--- js/index.js
+++ js/index.js
@@ -1,22 +1,38 @@
 window.onload = function() {
-	let btnStart = document.getElementById("btn-start");
-	let homeCharacter = document.getElementById("hero-character");
-	let items = [btnStart, homeCharacter];
-	let heroWrap = document.getElementById("hero-wrap");
+	let btnStart = document.getElementById('btn-start');
+	let homeCharacter = document.getElementById('hero-character');
+	let itemsToRemove = [btnStart, homeCharacter];
+	let heroWrap = document.getElementById('hero-wrap');
+	let canvasTag = 'canvas';
+	let canvasId = 'canvas';
+	let container = document.getElementById('container');
+	
 
-	function removeHomeItems (itemsToRemove){
-		itemsToRemove.forEach(item => {
+	let removeHomeItems = (items) => {
+		items.forEach(item => {
 			item.remove();
 		});
 	}
 
-	function makeTitleSmaller(title) {
-		title.classList.add('small');
+	let makeTitleSmaller = (hero) => {
+		hero.classList.add('small');
 	}
 
+	let addHtmlItems = (canvas, canvasId) => {
+		canvasElem = document.createElement(canvas);
+		canvasElem.setAttribute('id', canvasId);
+		container.appendChild(canvasElem);
+	}
+	
 	btnStart.addEventListener('click', function(e){
 		e.preventDefault();
-		removeHomeItems(items);
+		removeHomeItems(itemsToRemove);
 		makeTitleSmaller(heroWrap);
+		addHtmlItems(canvasTag, canvasId);
+		/* NEW GAME */
+		let game = new Game(canvasId);
+		game.start();
 	});
+
+	
 }
