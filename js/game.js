547909f8d4f39d9cb4e3595220801435d7670375
agdwm <almudenaguerraswm@gmail.com>
2018-09-08 17:43:53 +0200

    Created the function that set the attributes to the canvas

--- js/game.js
+++ js/game.js
@@ -1,15 +1,13 @@
-function Game(canvasId) {
+function Game (canvasId) {
 	this.canvas = document.getElementById(canvasId);
-	this.canvas.width = window.innerWidth;
-	this.canvas.height = window.innerHeight;
 	this.ctx = this.canvas.getContext('2d');
 	this.fps = 60;
 }
 
-Game.prototype.reset = function(){
+Game.prototype.reset = function() {
 
 }
 
-Game.prototype.start = function(){
+Game.prototype.start = function() {
 
 }
--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
 window.onload = function() {
+
 	let btnStart = document.getElementById('btn-start');
 	let homeCharacter = document.getElementById('hero-character');
-	let itemsToRemove = [btnStart, homeCharacter];
 	let heroWrap = document.getElementById('hero-wrap');
-	let canvasTag = 'canvas';
-	let canvasId = 'canvas';
 	let container = document.getElementById('container');
-	
+
+	let itemsToRemove = [btnStart, homeCharacter];
+
+	let canvasId = 'canvas';
+	let canvasWidth = window.innerWidth;
+	let canvasHeight = window.innerHeight;
+	let canvasAttrs = {
+		width: canvasWidth,
+		height: canvasHeight,
+		id: canvasId,
+		class: canvasId
+	}
 
 	let removeHomeItems = (items) => {
 		items.forEach(item => {
@@ -18,21 +27,30 @@ window.onload = function() {
 		hero.classList.add('small');
 	}
 
-	let addHtmlItems = (canvas, canvasId) => {
-		canvasElem = document.createElement(canvas);
-		canvasElem.setAttribute('id', canvasId);
+	let addHtmlElem = (canvasIdentifier) => {
+		let canvasElem = document.createElement(canvasIdentifier);
 		container.appendChild(canvasElem);
+	}
+
+	let setAttrs = (elem, attrs) => {
+		for (let key in attrs) {
+			elem[0].setAttribute(key, attrs[key]);	
+		}
+	}
+
+	/* NEW GAME */
+	let createNewGame = () => {
+		let canvasTag = document.getElementsByTagName(canvasId);
+		setAttrs(canvasTag, canvasAttrs);
+		let game = new Game(canvasId);
+		game.start();
 	}
 	
 	btnStart.addEventListener('click', function(e){
 		e.preventDefault();
 		removeHomeItems(itemsToRemove);
 		makeTitleSmaller(heroWrap);
-		addHtmlItems(canvasTag, canvasId);
-		/* NEW GAME */
-		let game = new Game(canvasId);
-		game.start();
+		addHtmlElem(canvasId);
+		createNewGame();
 	});
-
-	
 }
